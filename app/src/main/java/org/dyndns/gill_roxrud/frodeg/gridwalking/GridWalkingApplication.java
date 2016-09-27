f932220950ffb012e39eb5ee4febd7da6a9bef34
Frode Roxrud Gill <frodegill@gmail.com>
2016-09-27 08:35:48 +0200

    Added Application (based on work by OsmDroid) to catch runtime exceptions

---
+++ app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/GridWalkingApplication.java
@@ -0,0 +1,28 @@
+package org.dyndns.gill_roxrud.frodeg.gridwalking;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+
+public class GridWalkingApplication extends Application{
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Thread.currentThread().setUncaughtExceptionHandler(new GridWalkingUncaughtExceptionHandler());
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    public static class GridWalkingUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(Thread thread, Throwable ex) {
+            Log.e("UncaughtException", "Got an uncaught exception: "+ex.toString());
+            ex.printStackTrace();
+        }
+    }
+}
