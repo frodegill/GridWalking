1e6ee48f59f8c45121000e83e8a91569e06e49a2
Frode Roxrud Gill <frodegill@gmail.com>
2017-01-22 17:21:49 +0100

    Remove unused code

--- app/src/test/java/org/dyndns/gill_roxrud/frodeg/gridwalking/PersistUnitTest.java
+++ app/src/test/java/org/dyndns/gill_roxrud/frodeg/gridwalking/PersistUnitTest.java
@@ -2,7 +2,6 @@ package org.dyndns.gill_roxrud.frodeg.gridwalking;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
