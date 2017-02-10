7d54a18481fbc1ef96c0b3aa79250a0b7a37d1f8
Frode Roxrud Gill <frodegill@gmail.com>
2017-02-10 23:17:48 +0100

    New version of osmdroid does not need EXTERNAL_STORAGE pemission anymore. Also fixed Screen Overlay error

--- app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/MainActivity.java
+++ app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/MainActivity.java
@@ -43,17 +43,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean needsPermissions() {
         if (Build.VERSION.SDK_INT >= 23) {
             List<String> permissions = new ArrayList<>();
-            String message = "Requested permissions:";
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
-                message += "\nLocation to show user location.";
-            }
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                message += "\nStorage access to store map tiles.";
             }
             if (!permissions.isEmpty()) {
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
                 String[] params = permissions.toArray(new String[permissions.size()]);
                 requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
                 return true;
@@ -69,24 +62,17 @@ public class MainActivity extends AppCompatActivity {
                 Map<String, Integer> perms = new HashMap<>();
                 // Initial
                 perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
                 // Fill with results
                 for (int i = 0; i < permissions.length; i++)
                     perms.put(permissions[i], grantResults[i]);
-                // Check for ACCESS_FINE_LOCATION and WRITE_EXTERNAL_STORAGE
+                // Check for ACCESS_FINE_LOCATION
                 Boolean location = perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-                Boolean storage = perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-                if (location && storage) {
+                if (location) {
                     // All Permissions Granted
                     Toast.makeText(MainActivity.this, "All permissions granted", Toast.LENGTH_SHORT).show();
-                } else if (location) {
-                    Toast.makeText(MainActivity.this, "Storage permission is required to store map tiles to reduce data usage and for offline usage.", Toast.LENGTH_LONG).show();
-                } else if (storage) {
-                    Toast.makeText(MainActivity.this, "Location permission is required to show the user's location on map.", Toast.LENGTH_LONG).show();
-                } else { // !location && !storage case
+                } else { // !location
                     // Permission Denied
-                    Toast.makeText(MainActivity.this, "Storage permission is required to store map tiles to reduce data usage and for offline usage." +
-                            "\nLocation permission is required to show the user's location on map.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Location permission is required to show the user's location on map.", Toast.LENGTH_SHORT).show();
                 }
             }
             break;
