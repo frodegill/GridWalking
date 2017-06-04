af7026206c0e6425783ca0acd314495279b99bcb
Frode Roxrud Gill <frodegill@gmail.com>
2017-06-05 00:05:21 +0200

    Minor tweaks

--- app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/networking/Networking.java
+++ app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/networking/Networking.java
@@ -21,16 +21,17 @@ public class Networking {
         URL url = new URL(urlString);
 
         HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY);
+        httpConnection.setReadTimeout(30*1000);
+        httpConnection.setConnectTimeout(5*1000);
+
         if (useSecureConnection) {
             HttpsURLConnection httpsConnection = (HttpsURLConnection) httpConnection;
             httpsConnection.setSSLSocketFactory(new TLSSocketFactory());
         }
 
-        httpConnection.setReadTimeout(30*1000);
-        httpConnection.setConnectTimeout(1*1000);
         httpConnection.setRequestMethod(requestMethod);
         httpConnection.setRequestProperty("Connection", "close");
-        httpConnection.setRequestProperty("User-Agent", "Grid Walking "+ BuildConfig.VERSION_NAME);
+        httpConnection.setRequestProperty("User-Agent", "Grid Walking/"+ BuildConfig.VERSION_NAME);
         httpConnection.setDoOutput(doOutput);
         httpConnection.setDoInput(doInput);
 
