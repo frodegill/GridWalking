b348727d9ed9337a4d9d0583a5636efd240a17b2
Frode Roxrud Gill <frodegill@gmail.com>
2017-09-03 16:22:42 +0200

    Removed unused imports

--- app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/Networking.java
+++ app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/Networking.java
@@ -1,16 +1,9 @@
 package org.dyndns.gill_roxrud.frodeg.gridwalking;
 
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.CoreProtocolPNames;
 
-import javax.net.ssl.SSLContext;
-
-import static org.apache.http.conn.ssl.SSLSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER;
 
 
 public class Networking {
