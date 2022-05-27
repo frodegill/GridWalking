295dfc786fde322a3f83cf94caf9d6498b9ff75d
Frode Roxrud Gill <frodegill@gmail.com>
2022-05-27 21:52:50 +0200

    Preparing for 1.17. Removing explicit (and deprecated) WRITE_EXTERNAL_STORAGE. Osmdroid should survive this now

--- app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/activities/MainActivity.java
+++ app/src/main/java/org/dyndns/gill_roxrud/frodeg/gridwalking/activities/MainActivity.java
@@ -131,9 +131,6 @@ public class MainActivity extends AppCompatActivity {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
             }
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            }
             if (!permissions.isEmpty()) {
                 String[] params = permissions.toArray(new String[0]);
                 requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_APP_PERMISSIONS);
@@ -170,7 +167,6 @@ public class MainActivity extends AppCompatActivity {
                 Map<String, Integer> perms = new HashMap<>();
                 // Initial
                 perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
                 // Fill with results
                 for (int i = 0; i < permissions.length; i++)
                     perms.put(permissions[i], grantResults[i]);
@@ -178,15 +174,6 @@ public class MainActivity extends AppCompatActivity {
                 if (perms.get(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // Permission Denied
                     final String msg = "Location permission is required to show the user's location on map.";
-                    if (toastView != null) {
-                        toastView.setText(msg);
-                    } else {
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    }
-                }
-                if (perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    // Permission Denied
-                    final String msg = "Write External Storage permission is required to store map tile cache.";
                     if (toastView != null) {
                         toastView.setText(msg);
                     } else {
